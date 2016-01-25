5df947a5f9b798879e24b1ac961d1bae60b98bec
Abigail Rolling and Brad London <arolling@gmail.com>
2016-01-25 14:57:37 -0800

    Add basic front end functionality

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,17 @@ function ToDoItem(task){
 ToDoItem.prototype.completeTask =function() {
   return this.completed = true;
 }
+// End business logic
+
+$(document).ready(function() {
+  $("form#todo").submit(function(event) {
+    event.preventDefault();
+
+    var newTask = $("input#new-todo").val();
+    var newToDo = new ToDoItem(newTask);
+    console.log(newToDo);
+
+    $("#toDoItems").append("<li>"+newToDo.task+"</li>");
+    $("input#new-todo").val("");
+  });
+});
